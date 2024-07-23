e86c882bb3336cbc4b1e6a3511f43d495a9bf861
Shine2028 <156256831+Shine2028@users.noreply.github.com>
2024-07-23 16:12:33 +0100

    finally

--- java/script.js
+++ java/script.js
@@ -6,4 +6,13 @@ function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
   }
-  
+  $(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+            $("#navbar").removeClass("transparent").addClass("scrolled");
+        } else {
+            $("#navbar").removeClass("scrolled").addClass("transparent");
+        }
+    });
+});
